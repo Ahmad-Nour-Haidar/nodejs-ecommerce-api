121065e7343cb6ecdb8f83927f6a7934a6391ac6
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-19 13:13:03 +0300

    ✏️ Create App Server Using Express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Our API');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
