b17f86bebe6402066bc10267bc59152682c4e037
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-19 13:49:48 +0300

    ✏️ 6. Installing Mongoose and Connect to Our Database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const morgan = require('morgan');
 
+const mongoose = require('mongoose');
+
 const express = require('express');
 
 const app = express();
@@ -13,6 +15,13 @@ if (process.env.NODE_ENV === 'development') {
 
 app.get('/', (req, res) => {
     res.send('Our API');
+});
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then((conn) => {
+        console.log(`MongoDB Connected: ${conn.connections[0].host}`);
+    }).catch((err) => {
+    console.log(`MongoDB Connection Error: ${err}`);
 });
 
 const PORT = process.env.PORT || 3000;
