07ff6b2d11ace7a5178c5fdf23f162e35464187a
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-19 23:57:39 +0300

    ✏️ 7. Handle Errors in Development and Production ENV

--- middlewares/error_middleware.js
+++ middlewares/error_middleware.js
@@ -1,12 +1,11 @@
 const globalError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    return res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack,
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorForDev(err, res);
+    } else {
+        sendErrorForProd(err, res);
+    }
 };
 
 const sendErrorForDev = (err, res) => {
