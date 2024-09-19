e1c4d5a91aa0b6b068ac7c74344c1efbfc142ad4
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-19 16:49:24 +0300

    ✏️ 1. Category Schema and Model

--- models/category_model.js
+++ models/category_model.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
+// 1- Create Schema
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Category required'],
+            unique: [true, 'Category must be unique'],
+            minlength: [3, 'Too short category name'],
+            maxlength: [32, 'Too long category name'],
+        },
+        // A and B => shopping.com/a-and-b
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+        image: String,
+    },
+    {timestamps: true}
+);
 
-const categorySchema = new mongoose.Schema({
-    name: String,
-});
-
+// 2- Create model
 const CategoryModel = mongoose.model('Category', categorySchema);
 
 module.exports = CategoryModel;
