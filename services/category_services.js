b0360a1928c42192b153c1b3c016c5fe6c090059
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-19 17:26:09 +0300

    ✏️ 4. Get All Categories

--- services/category_services.js
+++ services/category_services.js
@@ -1,18 +1,24 @@
 const slugify = require('slugify');
 const asyncHandler = require('express-async-handlr')
 
-const CategoryModel = require('../models/category_model');
+const Category = require('../models/category_model');
 
-const getCategories = async function getCategories(req, res) {
-};
-
-const createCategory = asyncHandler(async (req, res) => {
-    const name = req.body.name;
-    const category = await CategoryModel.create({name, slug: slugify(name)});
-    res.status(201).json({data: category});
+// @desc    Get list of categories
+// @route   GET /api/v1/categories
+// @access  Public
+exports.getCategories = asyncHandler(async (req, res) => {
+    const categories = await Category.find();
+    res.status(200).json({
+        results: categories.length,
+        categories: categories
+    });
 });
 
-module.exports = {
-    getCategories,
-    createCategory,
-};
+// @desc    Create category
+// @route   POST  /api/v1/categories
+// @access  Private
+exports.createCategory = asyncHandler(async (req, res) => {
+    const name = req.body.name;
+    const category = await Category.create({name, slug: slugify(name)});
+    res.status(201).json({data: category});
+});
