e8fddc20f67abc77a9af5321e07dd03e5d71b32f
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-24 22:56:43 +0300

    ✏️ 4. Refactor Mount Routes Middleware

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const categoryRoutes = require("./category_routes");
+const subCategoryRoute = require("./subcategory_routes");
+const brandRoute = require("./brand_route");
+const productRoute = require("./product_route");
+const userRoute = require("./user_route");
+const authRoute = require("./auth_route");
+const reviewRoute = require("./review_route");
+const wishlistRoute = require("./wishlist_route");
+const addressRoute = require("./address_route");
+const couponRoute = require("./coupon_route");
+
+const mountRoutes = (app) => {
+    app.use('/api/v1/categories', categoryRoutes);
+    app.use('/api/v1/subcategories', subCategoryRoute);
+    app.use('/api/v1/brands', brandRoute);
+    app.use('/api/v1/products', productRoute);
+    app.use('/api/v1/users', userRoute);
+    app.use('/api/v1/auth', authRoute);
+    app.use('/api/v1/reviews', reviewRoute);
+    app.use('/api/v1/wishlist', wishlistRoute);
+    app.use('/api/v1/addresses', addressRoute);
+    app.use('/api/v1/coupons', couponRoute);
+};
+
+module.exports = mountRoutes;
--- server.js
+++ server.js
@@ -7,17 +7,7 @@ const ApiError = require('./utils/api_error');
 const globalError = require('./middlewares/error_middleware');
 
 // Routes
-const categoryRoutes = require('./routes/category_routes');
-const subCategoryRoute = require('./routes/subcategory_routes');
-const brandRoute = require('./routes/brand_route');
-const productRoute = require('./routes/product_route');
-const userRoute = require('./routes/user_route');
-const authRoute = require('./routes/auth_route');
-const reviewRoute = require('./routes/review_route');
-const wishlistRoute = require('./routes/wishlist_route');
-const addressRoute = require('./routes/address_route');
-const couponRoute = require('./routes/coupon_route');
-
+const mountRoutes = require('./routes');
 
 // db connection
 require('./config/database')();
@@ -33,16 +23,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Mount Routes
-app.use('/api/v1/categories', categoryRoutes);
-app.use('/api/v1/subcategories', subCategoryRoute);
-app.use('/api/v1/brands', brandRoute);
-app.use('/api/v1/products', productRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/reviews', reviewRoute);
-app.use('/api/v1/wishlist', wishlistRoute);
-app.use('/api/v1/addresses', addressRoute);
-app.use('/api/v1/coupons', couponRoute);
+mountRoutes(app);
 
 app.all('/', (req, res, next) => {
     next(new ApiError(`Can't find this route: ${req.originalUrl}`, 404));
