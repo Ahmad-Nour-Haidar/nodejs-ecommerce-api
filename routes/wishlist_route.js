de03865ca492ab559302ef20efe79c8cbc9a6dce
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-24 22:15:19 +0300

    ✏️ 16. Get Logged User Wishlist

--- routes/wishlist_route.js
+++ routes/wishlist_route.js
@@ -5,15 +5,16 @@ const authService = require('../services/auth_service');
 const {
     addProductToWishlist,
     removeProductFromWishlist,
+    getLoggedUserWishlist,
 } = require('../services/wishlist_service');
 
 const router = express.Router();
 
 router.use(authService.protect, authService.allowedTo('user'));
 
-router
-    .route('/')
-    .post(addProductToWishlist);
+router.route('/')
+    .post(addProductToWishlist)
+    .get(getLoggedUserWishlist);
 
 router.delete('/:productId', removeProductFromWishlist);
 
