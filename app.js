2e6a69e74106a2b825d67ec56986f3db77d495b9
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-26 20:03:41 +0300

    ✏️ 2. Set Request Size limit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post(
     webhookCheckout
 );
 
-app.use(express.json());
+app.use(express.json({ limit: '20kb' }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === 'development') {
