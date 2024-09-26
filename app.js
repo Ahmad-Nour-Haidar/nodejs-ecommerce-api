f99b9f9f9fefba1eccf8e9805c0ee280ac50977d
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-09-27 00:21:41 +0300

    🔨 Refactor code

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100,
     message:
-        'Too many accounts created from this IP, please try again after an hour',
+        'Too many accounts created from this IP, please try again after an 15 minutes',
 });
 
 // Apply the rate limiting middleware to all requests
